ab56e68d84b31af1f55e5cf8d1c5eedc2d732cec
clumsybird <yuwenyangfeng@126.com>
2018-10-09 18:49:29 +0800

    could set hour

--- BinaryClock.ino
+++ BinaryClock.ino
@@ -20,12 +20,13 @@ void BinaryClock_Set() // set time as you want
 {
 	while (digitalRead(SET) == LOW)
 	{
-		//while (digitalRead(SET) == LOW);//eliminate dithering, cancel it when proteus VSM debug
+		while (digitalRead(SET) == LOW);//eliminate dithering, cancel it when proteus VSM debug
 		int _hr = hour();
-		unsigned long previous = millis();
+		unsigned long previous = millis()-500;
 		do
 		{
 			BinaryClock_Hour(_hr); // display hour-in set mode
+			BinaryClock_Seconds(0);
 			while ((digitalRead(ADD) == LOW) && ((millis() - previous) > 500))
 			{
 				if (_hr < 11)
@@ -42,5 +43,7 @@ void BinaryClock_Set() // set time as you want
 		}
 		while (digitalRead(SET) == HIGH); // when set is HIGH, cycle in do...while
 		setTime(_hr, 0, 0, 12, 12, 2012);
+		while (digitalRead(SET) == LOW);//eliminate dithering, cancel it when proteus VSM debug
 	}
+
 }
