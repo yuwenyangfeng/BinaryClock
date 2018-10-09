ef0e6d7a10538cb2b32914b24fe2122baa535241
Clumsybird <yuwenyangfeng@126.com>
2018-10-09 08:40:50 +0800

    proteus debug can not go in void BinaryClock_Set(); do..while.so pick this block to main.ino

--- BinaryClock.cpp
+++ BinaryClock.cpp
@@ -12,54 +12,7 @@ void BinaryClock_Port()
 	pinMode(ADD, INPUT_PULLUP); // Add Button pull-up
 }
 
-// method:SET TIME
-void BinaryClock_Set() // set time as you want
-{
-	if (digitalRead(SET) == LOW)
-	{
-		BinaryClock_Seconds(0); // turn off Seconds' display-dropped in set mode
-		bool _flag = false; // --tag forbid jump out SET MODE
-		int _hr = hour();
-		int _min = minute();
-		do
-		{
-			BinaryClock_Hour(_hr); // display hour-in set mode
-			if (digitalRead(ADD) == LOW)
-			// add button be trigger
-			{
-				_flag = true; // --overturn tag, can jumpout SET MODE
-				if (_hr < 11)
-					_hr = _hr + 1;
-				else
-					_hr = 0;
-				delay(300); // Elimination Buffeting of Keystroke
-			}
-			if ((digitalRead(SET)) == LOW && (_flag == true))
-			// when Set Button is LOW && '_flag' tag is TRUE status,can jump out SET MODE
-			break; // break out 'do...while'
-		}
-		while (1);
-		_flag = false; //
-		do
-		{
-			BinaryClock_Minute(_min);
-			if (digitalRead(ADD) == LOW)
-			{
-				_flag = true;
-				if (_min < 59)
-					_min = _min + 1;
-				else
-					_min = 0;
-				delay(300);
-			}
-			if ((digitalRead(SET)) == LOW && (_flag == true))
-				break;
-		}
-		while (1);
-		setTime(_hr, _min, 0, 12, 12, 2012); 
-		while (digitalRead(SET) == LOW); // when button push unback,creat a sustain LOW level, main programe will drop in next circle.so,if button un srping-back,stay in this sentence.
-	}
-}
+
 
 // method: octal is binary too on pc, use bit-move opration.
 void BinaryClock_Hour(int Hr)
--- BinaryClock.ino
+++ BinaryClock.ino
@@ -9,8 +9,30 @@ void setup()
 
 void loop()
 {
-	BinaryClock_Set();
+	// method:SET TIME
+	void BinaryClock_Set();
 	BinaryClock_Hour(hour());
 	BinaryClock_Minute(minute());
 	BinaryClock_Seconds(second());
+}
+
+void BinaryClock_Set() // set time as you want
+{
+	int _hr = hour();
+	BinaryClock_Seconds(0); // turn off Seconds' display-dropped in set mode
+	do
+	{
+		BinaryClock_Hour(_hr); // display hour-in set mode
+		if (digitalRead(ADD) == LOW)
+		// add button be trigger
+		{
+			if (_hr < 11)
+				_hr = _hr + 1;
+			else
+				_hr = 0;
+			delay(300); // Elimination Buffeting of Keystroke
+		}
+	}
+	while (digitalRead(SET) == HIGH);
+	while (digitalRead(SET) == LOW);
 }
